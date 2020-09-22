234f8752822c0389cd9c0e726c686efedf58988e
floyd <facundomartinezesc@gmail.com>
2020-09-22 18:44:50 -0300

    Set up completed

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const chalk = require("chalk");
+const morgan = require("morgan");
+const nunjucks = require("nunjucks");
+const app = express();
+
+app.set("view engine", "html");
+app.engine("html", nunjucks.render);
+nunjucks.configure("views");
+
+app.use(morgan("tiny"));
+
+app.use(function (req, res, next) {
+  const data = [{ name: "Full" }, { name: "Stacker" }, { name: "Son" }];
+  res.render("index", { title: "Hall of Fame", personas: data });
+});
+
+app.use(express.static("./public"));
+
+app.use(function (req, res, next) {
+  console.log(chalk.yellow(req));
+  next();
+});
+app.use("/special/", function (req, res, next) {
+  console.log(chalk.blue("llegaste a un área especial"));
+  next();
+});
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000, function () {
+  console.log("Servidor corriendo en el puerto 3000");
+});
